92b1f07a44f53b61416a514dde78165fc593ec5c
yossmgaming <reddamage191913@gmail.com>
2026-02-21 19:54:13 +0530

    fix: increase pwa cache limit to fix vercel build

--- ssd-react/vite.config.js
+++ ssd-react/vite.config.js
@@ -35,6 +35,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5000000,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         navigateFallback: 'index.html',
         runtimeCaching: [
